09e918fc9c1466980eeb46d49c3a0d6cb3436bca
JUSTICE MENSAH BLAY MEWUBE <105025595+JUSTICEMENSAH-ALX@users.noreply.github.com>
2023-03-22 22:50:48 +0000

    UPDATES

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,14 @@
-/* eslint-disable */
-export default function getResponseFromAPI() {
-	return new Promise((resolve, reject) => {});
+// Only make Promise
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    /* eslint-disable */
+      if (true) {
+        resolve();
+      } else {
+        reject();
+      }
+      /* eslint-enable */
+  });
 }
+
+export default getResponseFromAPI;
